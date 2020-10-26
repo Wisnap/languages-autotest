d37ffd234950ec0773b93e8c7eec0c8c56109c94
Pavel Vishnevskii <wisnap@mail.ru>
2020-10-27 00:47:58 +0300

    WIP: made only for Chrome, add option type

--- conftest.py
+++ conftest.py
@@ -4,28 +4,21 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    # parser.addoption('--browser_name', action='store', default=None,
+    #                 help="Choose browser: chrome or firefox")
+
     parser.addoption('--language', action='store', default=None,
                      help="Choose language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser_locale = 'es'
-        options = Options()
-        options.add_argument("--lang={}".format(browser_locale))
-        browser = webdriver.Chrome(chrome_options=options)
+    print("\nstart chrome browser for test..")
+    browser_locale = request.config.getoption("language")
+    options = Options()
+    options.add_argument("--lang={}".format(browser_locale))
+    browser = webdriver.Chrome(chrome_options=options)
 
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
