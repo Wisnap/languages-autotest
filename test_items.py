1ff24dfc6ed63039596efde343b505cd0f12b0ab
Pavel Vishnevskii <wisnap@mail.ru>
2020-10-27 01:21:50 +0300

    delete not needed files

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_see_correct_language(browser):
+def test_see_correct_espanol_language(browser):
     browser.get(link)
     #time.sleep(30)
     button = browser.find_element_by_class_name("btn-add-to-basket")
