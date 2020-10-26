da11cc2c0cd04d8ce0c4832964644881f06dbb78
Pavel Vishnevskii <wisnap@mail.ru>
2020-10-27 01:46:56 +0300

    fixed issues

--- test_items.py
+++ test_items.py
@@ -3,10 +3,8 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_see_correct_espanol_language(browser):
+def test_check_foreign_button_on_page(browser):
     browser.get(link)
     #time.sleep(30)
-    button = browser.find_element_by_class_name("btn-add-to-basket")
-    value = button.text
-    assert value in "Añadir al carrito", "Incorrect text in button!"
+    assert browser.find_element_by_class_name("btn-add-to-basket"), "No button on page!"
     # line at the end
