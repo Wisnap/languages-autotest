e3cb01c2dfdf9ee4a1f65d6031e85a85c6a42491
Pavel Vishnevskii <wisnap@mail.ru>
2020-10-27 01:17:49 +0300

    test_passed

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 import time
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_see_correct_language(browser):
     browser.get(link)
-    time.sleep(5)
-    browser.find_element_by_css_selector("#login_link")
+    #time.sleep(30)
+    button = browser.find_element_by_class_name("btn-add-to-basket")
+    value = button.text
+    assert value in "Añadir al carrito", "Incorrect text in button!"
