9516d0594e489e9c6bd22ca220aab0c9ec1ea373
Pavel Vishnevskii <wisnap@MacBook-Pro-Pavel.local>
2020-10-27 10:10:52 +0300

    test_items.py

--- test_items.py
+++ test_items.py
@@ -5,6 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_foreign_button_on_page(browser):
     browser.get(link)
+    element = browser.find_element_by_class_name("btn-add-to-basket")
     #time.sleep(30)
-    assert browser.find_element_by_class_name("btn-add-to-basket"), "No button on page!"
+    assert element.is_displayed(), "No button on page!"
     # line at the end
